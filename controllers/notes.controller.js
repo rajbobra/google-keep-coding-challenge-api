8971c751961efc65e52fd5b1491c43cb32585a44
Raj Bobra <rajbobra.py@gmail.com>
2024-05-13 00:01:02 -0700

    Added Delete endpoint

--- controllers/notes.controller.js
+++ controllers/notes.controller.js
@@ -1,12 +1,14 @@
+const { where } = require('sequelize');
 const sql = require('../config/database');
 const { sequelize, Note } = require('../model/note')
 const http = require('http');
+const { OK } = require('sqlite3');
 
 class NotesController {
     static async getAllNotes(request, response) {
         try {
             const notes = await Note.findAll({order: [['updatedAt', 'DESC']]});
-            response.json(notes);
+            response.status(200).json(notes);
         } catch(error) {
             console.error(error);
             response.status(500).json({ message: 'Server Error getting notes from DB' });
@@ -17,9 +19,18 @@ class NotesController {
         try {
             const { id, title, content } = request.body;
             const note = await Note.create({id, title, content});
-            // response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
-            // response.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-            response.json(note);
+            response.status(200).json(note);
+        } catch(error) {
+            console.error(error);
+            response.status(500).json({ message: 'Server Error adding note' });
+        }
+    }
+
+    static async deleteNote(request, response) {
+        try {
+            const idToBeDeleted = request.params.id;
+            const note = await Note.destroy({where: {id: idToBeDeleted}});
+            response.status(200).json({ message: `Note with id ${idToBeDeleted} successfully deleted.` });
         } catch(error) {
             console.error(error);
             response.status(500).json({ message: 'Server Error adding note' });
--- routes/notes.router.js
+++ routes/notes.router.js
@@ -4,5 +4,6 @@ const NotesController = require("../controllers/notes.controller");
 
 router.get('/notes', NotesController.getAllNotes);
 router.post('/createNote', NotesController.postNote)
+router.delete('/deleteNote/:id', NotesController.deleteNote)
 
 module.exports = router;
